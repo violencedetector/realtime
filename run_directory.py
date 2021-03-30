c0a2e7c8f8fe5175024370378eaa1d0055d6ad25
violencedetector <75316854+violencedetector@users.noreply.github.com>
2021-03-30 18:16:41 -0300

    Update run_directory.py

--- run_directory.py
+++ run_directory.py
@@ -16,24 +16,7 @@ import numpy as np
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
 
-#from lifting.prob_model import Prob3dPose
-#from lifting.draw import plot_pose
 
-#import pickle#
-
-#def write(data, outfile):
-#        f = open(outfile, "w+b")
-#        pickle.dump(data, f)
-#        f.close()
-
-#def read(filename):
-#        f = open(filename)
-#        data = pickle.load(f, encoding='latin1')
-#        u = pickle._Unpickler(f)
-#        u.encoding = 'latin1'
-#        data = u.load()
-#        f.close()
-#        return data
 import numpy as np
 
 logger = logging.getLogger('TfPoseEstimator')
@@ -69,7 +52,7 @@ if __name__ == '__main__':
     
     total_indv=0
 
-    #individuals_full=np.array([])
+
 
     def find_back_forward(query,index):
         result=99
@@ -121,12 +104,7 @@ if __name__ == '__main__':
 #        read_data = read("poses.data")
 
         individuals_full.append(individuals)
-# TRATAR NUMERO DIFERENTE DE INDIVIDUOS, COMO FAZER?
-# IGNORAR POUCOS PONTOS? IDENTIFICAR Q NãO é PROXIMO DOS DEMAIS?
 
-
-
-        ##print(len(individuals))
 	
         if (i % DEPTH != 0) and last_dimension!=len(individuals): 
             print(" [incompatible dimension] "+file)
@@ -136,9 +114,6 @@ if __name__ == '__main__':
         list_individuals.append(len(individuals))##
                 
 
-        #individuals_full=np.vstack((individuals_full,individuals))
-
-       #np.append(individuals_full,individuals,0)
         if counter!= DEPTH:
             counter+=1
         else:
@@ -146,19 +121,10 @@ if __name__ == '__main__':
             most_common_item = max(list_individuals, key = list_individuals.count)##
             
             for counter_duplicate in range(DEPTH):		
-#                if counter_duplicate!=0:
-#                    if list_individuals[counter_duplicate]!=list_individuals[counter_duplicate-1]:#!!!!!!!comparar com o maximo comun, trocar #desde o primeiro / testar test.py no primeiro item!!!!!! o primeiro item não está comparando com o maximo
-#                        print("troquei: "+str(list_individuals[counter_duplicate])+"/"+str(list_individuals[counter_duplicate-1]))
-#                        individuals_reviwed.append(find_back_forward(most_common_item,counter_duplicate))
-#                        print("frame: "+str(counter_duplicate))
-#                        #print(len(individuals_reviwed))
-#                    else:
-#                        individuals_reviwed.append(individuals_full[counter_duplicate])
-#                else:
-#                    individuals_reviwed.append(individuals_full[counter_duplicate])
+
 
                 if list_individuals[counter_duplicate]!=most_common_item:
-                    ##print("troquei: "+str(list_individuals[counter_duplicate])+"item: "+str(counter_duplicate))
+                 
                     individuals_reviwed.append(find_back_forward(most_common_item,counter_duplicate))
                 else:
                     individuals_reviwed.append(individuals_full[counter_duplicate])
@@ -170,27 +136,13 @@ if __name__ == '__main__':
             with open(file+'.npy', 'wb') as f:
                 ##np.save(f,data)#troquei pelo reviewed
                 np.save(f,np.array(individuals_reviwed))
-#            with open(file+'.npy', 'rb') as f:
-#                read_data = np.load(f, allow_pickle=True)
-            #print(read_data)
-            #print(read_data.shape)
-##############
-#            f_log.write(" [normal] \n")#
-#            f_log.write(str(np.array(individuals_full)))#
-#            f_log.write(" [revisao] \n")#
-#            f_log.write(str(np.array(individuals_reviwed)))#
-#!!!!!!!!!!!!!!!!!!!!!!!!!! EXTRAIR SOMENTE OF FRAMES COM MESMA QTD DE INDIVIDUOS
-#                           POSSO AUMENTAR O FRAME RATE PRA EXTRAIR MAIS FRAMES
-#                            VOU TENTAR PASSAR INFRMAçAO DIRETO PRA LSTM
-#                           POSSIVELMENTE UTILIZAR DISTANCIA EUCLIDIANA DE CENTROIDES
-#                            OUTRA POSSIBILIDADE é IGNORAR DEPERMINADO NUMERO DE PONTOS, MAS 
-#                            O MSID PROVAVEL É Q A QRD MAIS COMUN NãO TENHA MUITA SUGEIRA
+
             f_log.write("Qtd: "+str(most_common_item))
             f_log.write("ocorrencia: "+str(list_individuals.count(most_common_item)))
 
             
             total_indv+=int(list_individuals.count(most_common_item))
-##############
+
             counter=1
             individuals_full=[]
             f_log.write(" [New file] \n")#
@@ -198,15 +150,7 @@ if __name__ == '__main__':
             list_individuals=[]##
             individuals_reviwed=[]##
 
-#	data=np.array(individuals_full)
-#	with open('test.npy', 'wb') as f:
-#	         np.save(f,data)
-#	with open('test.npy', 'rb') as f:
-#	         read_data = np.load(f)
-#	print(read_data)
-        ###
 
-#lista arqivos que pose nao foi identificada
         if humans!=[]:
                  a=1
         else:
@@ -216,8 +160,6 @@ if __name__ == '__main__':
     f_log.write(" [total individuals found] "+str(total_indv))
 
 
-#    with open(os.path.join(args.folder, 'pose.dil'), 'wb') as f:
-#        dill.dump(all_humans, f, protocol=dill.HIGHEST_PROTOCOL)
-    ##print(lista)
+
     f_log.close()#
     f.close()
